9298aad3d00f0ef41836270236493060daf9c9b4
Mateusz Brawański <emzi0767@gmail.com>
2018-04-14 13:02:10 +0000

    Enhanced build information section.

--- Makefile
+++ Makefile
@@ -2,8 +2,14 @@ CC=gcc
 LD=ld
 
 CFLAGS=-std=c99 -Wall
-TIMESTAMP=$(shell date -u +'\"%Y-%m-%dT%H:%M:%SZ\"')
-COMMIT=\"$(shell git rev-parse HEAD)\"
+
+TIMESTAMP="$(shell date -u +'L\"%Y-%m-%dT%H:%M:%SZ\"')"
+COMMIT="L\"$(shell git rev-parse HEAD)\""
+WORKDIR="L\"$(shell pwd)\""
+MACHINE="L\"$(shell hostname)\""
+USER="L\"$(shell whoami)\""
+COMPILER="L\"$(shell $(CC) --version | head -n1)\""
+HOST="L\"$(shell uname -a)\""
 
 ODIR=.
 ONAME=stegman
@@ -16,7 +22,7 @@ OBJS = $(OBJ)sha256.o $(OBJ)aes.o $(OBJ)zlib.o $(OBJ)steg.o $(OBJ)png.o $(OBJ)pr
 $(OBJ)%.o: %.c $(DEPS)
 	@[ -d $(OBJ) ] || mkdir -p $(OBJ)
 	@echo " [ CC ] " $@
-	@$(CC) -c -o $@ $< $(CFLAGS) -finput-charset=UTF-8 -D__TIMESTAMP_ISO__=$(TIMESTAMP) -D__GIT_COMMIT__=$(COMMIT)
+	@$(CC) -c -o $@ $< $(CFLAGS) -finput-charset=UTF-8 -D__BUILDINFO__ -D__TIMESTAMP_ISO__=$(TIMESTAMP) -D__GIT_COMMIT__=$(COMMIT) -D__WORKDIR__=$(WORKDIR) -D__MACHINE__=$(MACHINE) -D__USER__=$(USER) -D__COMPILER__=$(COMPILER) -D__HOST__=$(HOST)
 
 $(ODIR)/$(ONAME): $(OBJS)
 	@[ -d $(ODIR) ] || mkdir -p $(ODIR)
--- program.c
+++ program.c
@@ -304,19 +304,15 @@ int32_t werrorf(const wchar_t* format, ...)
 
 void print_usage(char* progname)
 {
-#ifdef __TIMESTAMP_ISO__
-#ifdef __GIT_COMMIT__
-	werrorf(L"%ls v%ls by %ls\n%ls\nBuilt at %s from commit %s\nUsage:\n\n", PROGRAM_NAME, PROGRAM_VERSION, PROGRAM_AUTHOR, PROGRAM_DESCRIPTION, __TIMESTAMP_ISO__, __GIT_COMMIT__);
-#else  // __GIT_COMMIT__
-	werrorf(L"%ls v%ls by %ls\n%ls\nBuilt at %s\nUsage:\n\n", PROGRAM_NAME, PROGRAM_VERSION, PROGRAM_AUTHOR, PROGRAM_DESCRIPTION, __TIMESTAMP_ISO__);
-#endif // __GIT_COMMIT__
-#else  // __TIMESTAMP_ISO__
-	werrorf(L"%ls v%ls by %ls\n%ls\nUsage:\n\n", PROGRAM_NAME, PROGRAM_VERSION, PROGRAM_AUTHOR, PROGRAM_DESCRIPTION);
-#endif // __TIMESTAMP_ISO__
-	werrorf(L"In order to use %ls, you need to specify operation mode. The program has 2\nmodes: encode, decode. Described below are arguments for each available mode.\n\n", PROGRAM_NAME);
+	werrorf(L"%ls v%ls by %ls\n%ls\n\nUsage:\n\n", PROGRAM_NAME, PROGRAM_VERSION, PROGRAM_AUTHOR, PROGRAM_DESCRIPTION);
+	werrorf(L"In order to use %ls, you need to specify operation mode. The program has 2 modes: encode, decode. Described below are arguments for each available mode.\n\n", PROGRAM_NAME);
 	werrorf(L"%s encode <password> <target file> <message>\n%s encode <password> <target file> @<source file>\npassword       The password to secure your data before encoding.\ntarget file    The file in which the data will be encoded.\nmessage        Text message to encode in the file.\nsource file    File to encode in the file.\n\n", progname, progname);
 	werrorf(L"%s decode <password> <source file> [target file]\npassword       The password used to secure your encoded data.\nsource file    The file in which the data was encoded.\ntarget file    The file in which the decoded data will be placed.\n\n", progname);
 	werrorf(L"When decoding, and the encoded data comes from a file, you need to specify the target file.\n");
+		
+#ifdef __BUILDINFO__
+	werrorf(L"\nBuild info:\nTimestamp:   %ls\nCommit:      %ls\nPath:        %ls\nMachine:     %ls\nUser:        %ls\nCompiler:    %ls\nHost:        %ls\n", __TIMESTAMP_ISO__, __GIT_COMMIT__, __WORKDIR__, __MACHINE__, __USER__, __COMPILER__, __HOST__);
+#endif // __BUILDINFO__
 }
 
 void fail(int32_t code, wchar_t *format, ...)
